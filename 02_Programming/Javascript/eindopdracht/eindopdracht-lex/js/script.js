2a1e7481efb221811e1266c2cde7a250e007f83f
Lex <alext@disroot.org>
2022-06-27 17:58:07 +0200

    Clock: Fix hour movement

--- 02_Programming/Javascript/eindopdracht/eindopdracht-lex/js/script.js
+++ 02_Programming/Javascript/eindopdracht/eindopdracht-lex/js/script.js
@@ -8,7 +8,7 @@ function setClock() {
     const currentDate = new Date();
     const secondsRatio = currentDate.getSeconds() / 60;
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
-    const hoursRatio = (secondsRatio + currentDate.getHours()) / 12;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
     setRotation(hourHand, hoursRatio);
     setRotation(minuteHand, minutesRatio);
     setRotation(secondHand, secondsRatio);
