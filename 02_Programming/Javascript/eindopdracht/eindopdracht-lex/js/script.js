f185a5fc844ea434bf583b597ca7107acd4932ba
Lex <alext@disroot.org>
2022-06-27 17:21:27 +0200

    Create clock. To do: add timezone functionality.

---
+++ 02_Programming/Javascript/eindopdracht/eindopdracht-lex/js/script.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector('.clock__face--hour');
+const minuteHand = document.querySelector('.clock__face--minute');
+const secondHand = document.querySelector('.clock__face--second');
+
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (secondsRatio + currentDate.getHours()) / 12;
+    setRotation(hourHand, hoursRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(secondHand, secondsRatio);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setClock();
