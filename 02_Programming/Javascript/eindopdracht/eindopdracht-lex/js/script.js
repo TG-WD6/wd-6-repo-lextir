7e038a828eb6b4f78f6ec3e3eefd50948433e480
Lex <alext@disroot.org>
2022-06-29 13:55:14 +0200

    Create basic carousel. To do: multiple image carousel with animation

--- 02_Programming/Javascript/eindopdracht/eindopdracht-lex/js/script.js
+++ 02_Programming/Javascript/eindopdracht/eindopdracht-lex/js/script.js
@@ -1,47 +1,62 @@
-//*  CLOCK
+document.addEventListener("DOMContentLoaded", () => {
 
-// setInterval(setClock, 1000);
+  //*  CLOCK
 
-// const hourHand = document.querySelector('.clock__face--hour');
-// const minuteHand = document.querySelector('.clock__face--minute');
-// const secondHand = document.querySelector('.clock__face--second');
+setInterval(setClock, 1000);
 
-// function setClock() {
-//     const currentDate = new Date();
-//     const secondsRatio = currentDate.getSeconds() / 60;
-//     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
-//     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
-//     setRotation(hourHand, hoursRatio);
-//     setRotation(minuteHand, minutesRatio);
-//     setRotation(secondHand, secondsRatio);
-// }
+const hourHand = document.querySelector('.clock__face--hour');
+const minuteHand = document.querySelector('.clock__face--minute');
+const secondHand = document.querySelector('.clock__face--second');
 
-// function setRotation(element, rotationRatio) {
-//     element.style.setProperty('--rotation', rotationRatio * 360);
-// }
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(hourHand, hoursRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(secondHand, secondsRatio);
+}
 
-// setClock();
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setClock();
+
 
 //* CAROUSEL
 
-document.addEventListener("click", e => {
-    let arrow
-    if (e.target.matches(".carousel__controls")) {
-        arrow = e.target
-    } else {
-        arrow = e.target.closest(".carousel__controls")
-    }
-    if (arrow != null) onArrowClick(arrow)
-})
+  const slides = document.querySelectorAll('.carousel__slide');
+  const nextButton = document.querySelector('.carousel__button--next');
+  const prevButton = document.querySelector('.carousel__button--prev');
+  let currentSlide = 0;
 
-function onArrowClick(arrow) {
-    const slider = arrow.closest(".carousel").querySelector(".carousel__slider")
-    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index")
-    )
-    if (arrow.classList.contains("carousel__controls--prev")) {
-        slider.style.setProperty("--slider-index", sliderIndex - 1)
+
+  function nextSlide() {
+    // huidige slide wordt verborgen (class '--displayed' wordt verwijderd)
+    slides[currentSlide].className = 'carousel__slide';
+    // maak de volgende slide de currentSlide
+    currentSlide = (currentSlide + 1) % slides.length;
+    // nieuwe currentSlide wordt getoond (--displayed wordt toegevoegd)
+    slides[currentSlide].className = 'carousel__slide carousel__slide--displayed';
+  }
+
+  function prevSlide() {
+    slides[currentSlide].className = 'carousel__slide';
+    currentSlide = (currentSlide - 1) % slides.length;
+    if (currentSlide == -1) {
+      currentSlide = slides.length - 1;
     }
-    if (arrow.classList.contains("carousel__controls--next")) {
-        slider.style.setProperty("--slider-index", sliderIndex + 1)
-    }
-}
+    slides[currentSlide].className = 'carousel__slide carousel__slide--displayed';
+  }
+
+  nextButton.addEventListener("click", () => {
+    nextSlide();
+  });
+
+  prevButton.addEventListener("click", () => {
+    prevSlide();
+  });
+
+});
