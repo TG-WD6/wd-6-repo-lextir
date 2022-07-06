b9904c112fd77d2f66f8a29c63d09efc9443df65
Lex <alext@disroot.org>
2022-07-06 23:31:30 +0200

    Add animation and scroll indicator to carousel

--- 02_Programming/Javascript/eindopdracht/eindopdracht-lex/js/carousel2.js
+++ 02_Programming/Javascript/eindopdracht/eindopdracht-lex/js/carousel2.js
@@ -1,30 +1,3 @@
-//*  CLOCK
-
-setInterval(setClock, 1000);
-
-const hourHand = document.querySelector('.clock__face--hour');
-const minuteHand = document.querySelector('.clock__face--minute');
-const secondHand = document.querySelector('.clock__face--second');
-
-function setClock() {
-  const currentDate = new Date();
-  const secondsRatio = currentDate.getSeconds() / 60;
-  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
-  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
-  setRotation(hourHand, hoursRatio);
-  setRotation(minuteHand, minutesRatio);
-  setRotation(secondHand, secondsRatio);
-}
-
-function setRotation(element, rotationRatio) {
-  element.style.setProperty('--rotation', rotationRatio * 360);
-}
-
-setClock();
-
-
-//* CAROUSEL
-
 let items = document.querySelectorAll('.carousel .carousel__item');
 let dots = document.querySelectorAll('.carousel__indicators li');
 let currentItem = 0;
